eaca2e809f44bd94902896ee52c9a6ef7eb5e700
Satria Bahari <satriaabaharii@gmail.com>
2024-05-31 00:12:15 +0700

    feat: add user feature

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,10 +24,13 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
+            // 'name' => fake()->name(),
+            // 'email' => fake()->unique()->safeEmail(),
+            // 'password' => static::$password ??= Hash::make('password'),
+            'name' => 'test',
+            'email' => 'test@gmail.com',
             'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
+            'password' => 'test',
             'remember_token' => Str::random(10),
         ];
     }
