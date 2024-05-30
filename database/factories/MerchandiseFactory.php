8aa6ad933cf88365ac9604218bdec1075f9a0de5
Satria Bahari <satriaabaharii@gmail.com>
2024-05-31 00:11:12 +0700

    feat: add merchandise faeature

---
+++ database/factories/MerchandiseFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Merchandise>
+ */
+class MerchandiseFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => fake()->sentence(),
+            'description' => fake()->text(),
+            'price' => fake()->randomNumber(5, true),
+            'stock' => fake()->randomDigit()
+        ];
+    }
+}
---
+++ database/seeders/MerchandiseSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Merchandise;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class MerchandiseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Merchandise::factory()->count(10)->create();
+    }
+}
