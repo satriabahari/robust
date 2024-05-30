610f6a9ba5792f2f1d90215ce734e17671b8f9e9
Satria Bahari <satriaabaharii@gmail.com>
2024-05-31 00:11:48 +0700

    feat: add membership feature

---
+++ database/migrations/2024_05_30_152759_create_memberships_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('memberships', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->integer('price');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('memberships');
+    }
+};
