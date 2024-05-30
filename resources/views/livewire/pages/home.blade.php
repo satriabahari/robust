36ef613dce618de9fb20b5b175589e1b1bdf9d8a
Satria Bahari <satriaabaharii@gmail.com>
2024-05-31 00:14:17 +0700

    feat: add login and register feature

---
+++ resources/views/livewire/pages/home.blade.php
@@ -0,0 +1,3 @@
+<div class="text-blue-500">
+    <h1>Home</h1>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,19 @@
 <?php
 
-use App\Livewire\Counter;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('livewire.show-posts');
-});
+Route::view('/', 'welcome');
 
-Route::get('/counter', Counter::class);
+Route::view('dashboard', 'dashboard')
+    ->middleware(['auth', 'verified'])
+    ->name('dashboard');
+
+Route::view('home', 'home')
+    ->middleware(['auth', 'verified'])
+    ->name('home');
+
+Route::view('profile', 'profile')
+    ->middleware(['auth'])
+    ->name('profile');
+
+require __DIR__.'/auth.php';
