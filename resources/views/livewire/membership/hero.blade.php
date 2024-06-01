d477c1858fb71e000a95f8b7c83207f9da563ad5
Satria Bahari <satriaabaharii@gmail.com>
2024-06-01 15:34:55 +0700

    feat: add home feature

---
+++ resources/views/livewire/membership/hero.blade.php
@@ -0,0 +1,5 @@
+<x-container class="text-center dark:text-neutral-50 flex flex-col items-center justify-center gap-y-4">
+    <h3 class="uppercase text-xl font-extrabold">Membership</h3>
+    <h1 class="text-5xl font-bold uppercase w-full lg:w-3/4">THE ULTIMATE MEMBERSHIP EXPERIENCE</h1>
+    <p>Join Fitness First from only <span class="font-bold">Rp 399.000</span> a month.</p>
+</x-container>
