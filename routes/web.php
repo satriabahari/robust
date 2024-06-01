41c73a08862155fed3319b9938dad2959acb863d
Satria Bahari <satriaabaharii@gmail.com>
2024-06-01 15:35:06 +0700

    refactor: add some route

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,27 @@
 <?php
 
+use App\Http\Controllers\MembershipController;
+use App\Livewire\Membership\Membership;
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'welcome');
+Route::view('/welcome', 'welcome');
+
+Route::view('/', 'home')->name('home');
+
+Route::view('member', 'member')->name('member');
+
+Route::get('membership', [MembershipController::class, 'index'])->name('membership');
+
+Route::view('join-online', 'join-online')->name('join-online');
+
+Route::view('merchandise', 'merchandise')->name('merchandise');
 
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
     ->name('dashboard');
 
-Route::view('home', 'home')
-    ->middleware(['auth', 'verified'])
-    ->name('home');
-
 Route::view('profile', 'profile')
     ->middleware(['auth'])
     ->name('profile');
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
