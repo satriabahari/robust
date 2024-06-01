21de9aae252eb9bf8848b97e4413102e09adc166
Satria Bahari <satriaabaharii@gmail.com>
2024-06-01 15:35:58 +0700

    style: add dark mode

---
+++ app/Livewire/Components/DarkModeToggle.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Components;
+
+use Livewire\Component;
+
+class DarkModeToggle extends Component
+{
+    public function render()
+    {
+        return view('livewire.components.dark-mode-toggle');
+    }
+}
